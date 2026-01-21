31a81b85f3ae52448ff73c28344edd8a4193fe63
dpetrovicc <dusan.petrovic.it@gmail.com>
2026-01-21 17:56:26 +0100

    Instaliran React i Tailwind v3

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-xl shadow-2xl text-center">
+        <h1 className="text-4xl font-extrabold text-blue-600 mb-4">
+          Handle Time ⏳
+        </h1>
+        <p className="text-gray-600 text-lg">
+          Pocetni Frontend
+        </p>
+        <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition">
+          Pocetak
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
